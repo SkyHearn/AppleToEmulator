e4c56ef85e6788318403bf94b0aa9f498997d78c
DavidHearn <david.c.hearn@outlook.com>
2021-05-20 10:48:32 -0700

    Fixed LDA and initCPU. Also Changed the memory to reflect the original Apple II
    LDA and initCPU needed to use pointers.  I needed to fix some dumb errors like using the '=' operator in conditionals. To do bitmasks I had to make it so the A register is copied instead of directly worked on.

--- AppleToEmulator/AppleToEmulator/AppleToEmulator.cpp
+++ AppleToEmulator/AppleToEmulator/AppleToEmulator.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
+using Byte = unsigned char;
+using Word = unsigned short;
+
 struct CPU 
 {
-	using Byte = unsigned char;
-	using Word = unsigned short;
-
 	Word PC;        // program counter
 	Byte SP = 0;    // stack pointer. add 0x0100 to it to get the stack location.
 
@@ -18,36 +18,50 @@ struct CPU
 	Byte O : 1;  // Overflow Flag
 	Byte N : 1;  // Negitive Flag.
 
-	Byte mem[64000] = {0};
+	Byte mem[4000] = {0};
 };
 
 // initlize the CPU
-void initCPU(CPU cpu)
+void initCPU(CPU* cpu)
 {
-	cpu.C = 0;
-	cpu.Z = 0;
-	cpu.I = 0;
-	cpu.D = 0;
-	cpu.B = 0;
-	cpu.O = 0;
-	cpu.N = 0;
+	cpu->C = 0;
+	cpu->Z = 0;
+	cpu->I = 0;
+	cpu->D = 0;
+	cpu->B = 0;
+	cpu->O = 0;
+	cpu->N = 0;
 
-	cpu.PC = 0x200; // TEMPORARY THIS IS NOT HOW THE APPLE II WORKS
+	cpu->mem[0x200] = 0xa9;
+	cpu->mem[0x201] = 0xff;
+
+	cpu->PC = 0x200; // TEMPORARY THIS IS NOT HOW THE APPLE II WORKS
 }
 
-void execute(CPU cpu, unsigned int cycles) 
+void execute(CPU* cpu, unsigned int cycles) 
 {
 	while (cycles != 0) 
 	{
-		switch (cpu.mem[cpu.PC])
+		switch (cpu->mem[cpu->PC])
 		{
+
+		// LDA immidiate addressing mode
 		case 0xa9:
-			cpu.A = cpu.mem[cpu.PC + 1];
-			if (cpu.A = 0) cpu.Z = 1;
-			if (0b00000001 & cpu.A) cpu.Z = 1;
+		{
+			cpu->A = cpu->mem[cpu->PC + 1];
+			Byte A_cpy = cpu->A; // copy A so that we can do bitwise on it.
+
+			if (cpu->A == 0) cpu->Z = 1; // set the zero flag
+			if ((0b00000001 & A_cpy) == 0b00000001) cpu->N = 1; // set the negitive flag if b7 is 1
+
 			--cycles;
-			--cycles;
+			--cycles; // decriment the correct number of cycles
+
+			cpu->PC = cpu->PC += 2;  // move the program counter
+
 			break;
+		}
+
 		default:
 			std::cout << "Instruction not handled \n";
 		}
@@ -57,9 +71,9 @@ void execute(CPU cpu, unsigned int cycles)
 int main()
 {
 	CPU cpu;
-	initCPU(cpu);
+	initCPU(&cpu);
 
-	execute(cpu, 2);
+	execute(&cpu, 2);
 
 	return 0;
 }
